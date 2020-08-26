9cce3a0b5878bba61e4c8490fc971a448bd8f662
Ivan Putra Eriansya <eriansha.van@gmail.com>
2020-08-26 16:11:54 +0700

    Create rejected reason static data

---
+++ src/data.js
@@ -0,0 +1,90 @@
+export const REJECTED_REASON = [
+  {
+    label: "Cannot Verify Authenticity",
+    value: "cannot_verify_authenticity"
+  },
+  {
+    label: "Heavy signs of wear",
+    value: "worn_out"
+  },
+  {
+    label: "No brand",
+    value: "no_brand"
+  },
+  {
+    label: "Out of category list",
+    value: "out_of_category"
+  },
+  {
+    label: "Out of brand list",
+    value: "out_of_brand"
+  },
+  {
+    label: "Requested to return",
+    value: "requested_to_return"
+  },
+  {
+    label: "Stain",
+    value: "stain"
+  },
+  {
+    label: "Torn",
+    value: "torn"
+  },
+  {
+    label: "Expired (Makeup, Fragrance, Skincare)",
+    value: "expired"
+  },
+  {
+    label: "Repetitive Item",
+    value: "repetitive"
+  },
+  {
+    label: "Basic wear (e.g. basic t-shirt, commercial tees, legging)",
+    value: "basic_wear"
+  },
+  {
+    label: "Has gone through alteration",
+    value: "altered"
+  },
+  {
+    label: "Used Activewear/Sportswear/Swimwear",
+    value: "used_sportwear"
+  },
+  {
+    label: "Outside of Women Wear",
+    value: "not_women_wear"
+  },
+  {
+    label: "Out of price range",
+    value: "out_of_price_range"
+  },
+  {
+    label: "Sample item",
+    value: "sample_item"
+  },
+  {
+    label: "Button Missing",
+    value: "button_missing"
+  },
+  {
+    label: "Skin Peeling Off",
+    value: "skin_peeling_off"
+  },
+  {
+    label: "Ornament is Missing",
+    value: "ornament_is_missing"
+  },
+  {
+    label: "Authenticity",
+    value: "authenticity"
+  },
+  {
+    label: "Broken",
+    value: "broken"
+  },
+  {
+    label: "Scratch",
+    value: "scratch"
+  },
+]
